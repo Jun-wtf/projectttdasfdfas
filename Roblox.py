c765a40af89d2286abcb338eb9851bff2ce43d9f
Juun <a31171957a0@gmail.com>
2025-09-16 16:45:26 +0900

    Roblox.py !Update!
    섹스는 좀 심해서 Gender로 바꿈;;

--- Roblox.py
+++ Roblox.py
@@ -4,7 +4,7 @@ class Roblox:
         self.name = name
         self.r = r
         self.color = color
-        self.sex = sex
+        self.Gender = Gender
     def print_info(self):
         print(f"system : 당신의 이름은... {self.name} 입니다.")
         print(f"system : {self.name}님의 r 버전은 {self.r} 입니다.")
@@ -12,7 +12,7 @@ class Roblox:
         print(f"system : {self.name}님의 성별은 {self.sex} 입니다.")
     # def __del__(self):
     #     print(f"Server : {self.name}님 이/가 삭재되었습니다.")
-# [name] = Roblox('[name]', '[r6 / r15]', '[color]', '[sex]')
+# [name] = Roblox('[name]', '[r6 / r15]', '[color]', '[Gender]')
 # [name].print_info()
 Owner = Roblox('HD', '6', 'black', 'man')
 
