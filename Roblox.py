31f8d39571004e1751d5b78acc465e60d1eb6a63
Juun <a31171957a0@gmail.com>
2025-11-17 17:45:20 +0900

    Roblox.py 업데이트

--- Roblox.py
+++ Roblox.py
@@ -9,7 +9,7 @@ class Roblox:
         print(f"system : 당신의 이름은... {self.name} 입니다.")
         print(f"system : {self.name}님의 r 버전은 {self.r} 입니다.")
         print(f"system : {self.name}님의 몸의 색은 {self.color}입니다.")
-        print(f"system : {self.name}님의 성별은 {self.sex} 입니다.")
+        print(f"system : {self.name}님의 성별은 {self.gender} 입니다.")
     # def __del__(self):
     #     print(f"Server : {self.name}님 이/가 삭재되었습니다.")
 
